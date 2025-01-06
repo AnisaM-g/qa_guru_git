4962cb489e08ac09bc42f84712131546821b0813
Anisa Mordvinova <anisa_mordvinova@mail.ru>
2025-01-06 15:54:18 +0500

    add getMessage()

--- src/test/java/HelloWordTest.java
+++ src/test/java/HelloWordTest.java
@@ -1,5 +1,5 @@
 public class HelloWordTest {
-    prived static String msg = "Hello World!!!";
+    private static String msg = "Hello World!!!";
     public static void main(String[] args){
         System.out.println(msg);
     }
