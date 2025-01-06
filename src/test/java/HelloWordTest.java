75ddd631dd8f781f77a0689a99b66d4d6e2b50bb
Anisa Mordvinova <71696998+AnisaM-g@users.noreply.github.com>
2025-01-06 14:17:56 +0300

    Update HelloWordTest.java

--- src/test/java/HelloWordTest.java
+++ src/test/java/HelloWordTest.java
@@ -1,6 +1,5 @@
 public class HelloWordTest {
     prived static String msg = "Hello World!!!";
-    prived static String msg = "Hello World!!!";
     public static void main(String[] args){
         System.out.println(msg);
     }
